b82731928bd370f66e42713f924fbcef7f2769f6
ParagonInterrupt <97362639+rm09boug@users.noreply.github.com>
2025-10-29 21:35:20 -0400

    Update quartz.layout.ts
    attempt to hide tags

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.TagList(),
+    Component.TagList({display: none;}),
   ],
   left: [
     Component.PageTitle(),
