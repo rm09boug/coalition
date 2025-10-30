f2578637c9d007a0f734953b1d9bd28a7d9d2b6a
ParagonInterrupt <97362639+rm09boug@users.noreply.github.com>
2025-10-29 22:36:44 -0400

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.TagList({display: none}),
+    Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
